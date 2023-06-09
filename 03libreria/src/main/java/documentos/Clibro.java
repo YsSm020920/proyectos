267319a611a9d08bb3250e0decb68dead38fa792
Yisell <Yisell@LAPTOP-E3T4V97T>
2023-06-09 08:26:13 -0600

    Libreria

---
+++ 03libreria/src/main/java/documentos/Clibro.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package documentos;
+
+import java.io.*;
+import java.util.*;
+
+public class Clibro {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+    private String autor;
+    private String editorial;
+    private float precio;
+
+    public Clibro() {
+    }
+    
+    
+    
+    
+    
+}
